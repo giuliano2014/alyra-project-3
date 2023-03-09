acf3594ea20b99587b1641f33e9f1549f92f37e7
rem beuch <beucherie.r@gmail.com>
2023-03-09 12:26:32 +0100

    owner conditional

--- client/src/components/Header/HeaderContainer.jsx
+++ client/src/components/Header/HeaderContainer.jsx
@@ -1,11 +1,37 @@
+import useEth from "../../contexts/EthContext/useEth";
+import { useEffect, useState } from "react";
+
+
 const HeaderContainer = () => {
-  return (
-    <div>
-      <button>Connexion</button>
-      <h1>Voting Dapp</h1>
-      <h4>Address of user logged : 0x000...</h4>
-    </div>
-  );
+  const { state: { contract, accounts } } = useEth();
+  const [address, setAddress] = useState();
+  const [owner, setOwner] = useState();
+
+
+  useEffect(() => {
+    if (contract?.methods) {
+      setAddress(accounts[0])
+      ownerise()
+    }
+  }, [contract])
+
+  async function ownerise() {
+    const ownerAdress = await contract.methods.owner().call();
+    setOwner(ownerAdress)
+  }
+
+  if (address === owner) {
+    return (
+      <div>
+        <button>Connexion</button>
+        <h1>Voting Dapp</h1>
+        <h4>Address of user logged : {address}</h4>
+      </div>
+    );
+
+  } else {
+    return (<div>You are not the owner</div>)
+  }
 }
 
 export default HeaderContainer;
