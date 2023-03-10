1e571806daf79748e9bd651888b3ca76f2a05564
rem beuch <beucherie.r@gmail.com>
2023-03-10 14:13:20 +0100

    add Proposal ok

--- client/src/components/Voter/AddProposal.jsx
+++ client/src/components/Voter/AddProposal.jsx
@@ -2,9 +2,17 @@ import { useState } from "react";
 
 const AddProposal = ({ accounts, contract }) => {
     const [description, setDescription] = useState("");
+    const [error, setError] = useState()
+
 
     const handleAddProposal = async () => {
-        await contract.methods.addProposal(description).send({ from: accounts[0] });
+        contract.methods.addProposal(description).call({ from: accounts[0] })
+            .then(result => {
+                return contract.methods.addProposal(description).send({ from: accounts[0] });
+            })
+            .catch(error => {
+                setError(error.message.match(/revert (.*)/)[1]);
+            });
     };
 
     const handleInputChange = (event) => {
@@ -22,6 +30,8 @@ const AddProposal = ({ accounts, contract }) => {
                 onChange={handleInputChange}
             />
             <button onClick={handleAddProposal}>addProposal</button>
+            {error}
+
         </div>
     );
 };
