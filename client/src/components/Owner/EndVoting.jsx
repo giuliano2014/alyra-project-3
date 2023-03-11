4502c938129412f93c90470af5f9aee9a2837ea2
Giuliano Furgol <giulianofurgol@hotmail.com>
2023-03-11 08:20:04 +0100

    End voting session code review

--- client/src/components/Owner/EndVoting.jsx
+++ client/src/components/Owner/EndVoting.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
 const EndVoting = ({ accounts, contract }) => {
-    const [error, setError] = useState()
+    const [error, setError] = useState();
 
     const endVotingSession = () => {
         contract.methods.endVotingSession().call({ from: accounts[0] })
             .then(result => {
-                return contract.methods.endVotingSession().send({ from: accounts[0] });
+                contract.methods.endVotingSession().send({ from: accounts[0] });
             })
             .catch(error => {
                 setError(error.message.match(/revert (.*)/)[1]);
--- client/src/components/Owner/OwnerContainer.jsx
+++ client/src/components/Owner/OwnerContainer.jsx
@@ -9,35 +9,33 @@ import StartVoting from "./StartVoting";
 import TallyVotes from "./TallyVotes";
 
 const OwnerContainer = () => {
-  const { state: { accounts, contract } } = useEth();
-  const [isOwner, setIsOwner] = useState(false);
+    const { state: { accounts, contract } } = useEth();
+    const [isOwner, setIsOwner] = useState(false);
 
-  const getOwnerAddress = async () => {
+    const getOwnerAddress = async () => {
     if (!contract) return;
     const ownerAddress = await contract.methods.owner().call();
     accounts[0] === ownerAddress ? setIsOwner(true) : setIsOwner(false);
-  }
+    }
 
-  useEffect(() => { getOwnerAddress() }, [contract]);
+    useEffect(() => { getOwnerAddress() }, [contract]);
 
-  // if (!isOwner) return null;
+    // if (!isOwner) return null;
 
-  return (
-    <div>
-      <hr />
-      <h2>Owner</h2>
-      <AddVoter accounts={accounts} contract={contract} />
-      <StartProposal accounts={accounts} contract={contract} />
-      <EndProposal accounts={accounts} contract={contract} />
-      <StartVoting accounts={accounts} contract={contract} />
-      <div>
-        <EndVoting accounts={accounts} contract={contract} />
-      </div>
-      <div>
-        <TallyVotes accounts={accounts} contract={contract} />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <hr />
+            <h2>Owner</h2>
+            <AddVoter accounts={accounts} contract={contract} />
+            <StartProposal accounts={accounts} contract={contract} />
+            <EndProposal accounts={accounts} contract={contract} />
+            <StartVoting accounts={accounts} contract={contract} />
+            <EndVoting accounts={accounts} contract={contract} />
+            <div>
+                <TallyVotes accounts={accounts} contract={contract} />
+            </div>
+        </div>
+    );
 };
 
 export default OwnerContainer;
